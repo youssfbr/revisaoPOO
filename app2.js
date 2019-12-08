b5ad47d15e07b66e9a7ad646a8faf53409a8f832
Alisson Youssf <youssfbr@gmail.com>
2019-12-08 19:28:32 -0300

    lista de carros

--- app2.js
+++ app2.js
@@ -15,10 +15,49 @@ var Carro = /** @class */ (function () {
     };
     return Carro;
 }());
-var carroA = new Carro('Veloster', 3);
-console.log(carroA);
-carroA.acelerar();
-console.log(carroA);
-carroA.acelerar();
-carroA.acelerar();
-console.log(carroA);
+var Concessionaria = /** @class */ (function () {
+    function Concessionaria(endereco, listaDeCarros) {
+        this.endereco = endereco;
+        this.ListaDeCarros = listaDeCarros;
+    }
+    Concessionaria.prototype.fornecedorEndereco = function () {
+        return this.endereco;
+    };
+    Concessionaria.prototype.mostrarListaDeCarros = function () {
+        return this.ListaDeCarros;
+    };
+    return Concessionaria;
+}());
+var Pessoa = /** @class */ (function () {
+    function Pessoa(nome, carroPreferido) {
+        this.nome = nome;
+        this.carroPreferido = carroPreferido;
+    }
+    Pessoa.prototype.dizerNome = function () {
+        return this.nome;
+    };
+    Pessoa.prototype.dizerCarroPreferido = function () {
+        return this.carroPreferido;
+    };
+    Pessoa.prototype.comprarCarro = function (carro) {
+        this.carro = carro;
+    };
+    Pessoa.prototype.dizerCarroQueTem = function () {
+        return this.carro;
+    };
+    return Pessoa;
+}());
+// Criar carros
+var carroA = new Carro('Dodge Journey', 4);
+var carroB = new Carro('Veloster', 3);
+var carroC = new Carro('Cerato', 4);
+/*
+    Agora... Colocar esses objetos dentro de um array
+    e... associar esse array de objetos do tipo carro
+    ao atributo lista de carros do objeto objeto concessionária
+*/
+// Criar array, montar a lista de carros da concessionária
+var listaDeCarros = [carroA, carroB, carroC];
+var concessionária = new Concessionaria('Rua dos Tabajaras', listaDeCarros);
+// exibir lista de carros
+console.log(concessionária.mostrarListaDeCarros());
--- app2.ts
+++ app2.ts
@@ -21,12 +21,64 @@ class Carro {
     }
 }
 
-let carroA = new Carro('Veloster', 3)
-console.log(carroA)
+class Concessionaria {
+    private endereco: string
+    private ListaDeCarros: Carro[]
 
-carroA.acelerar()
-console.log(carroA)
+    constructor(endereco: string, listaDeCarros) {
+        this.endereco = endereco
+        this.ListaDeCarros = listaDeCarros
+    }
 
-carroA.acelerar()
-carroA.acelerar()
-console.log(carroA)
+    public fornecedorEndereco(): string {
+        return this.endereco
+    }
+
+    public mostrarListaDeCarros(): Carro[] {
+        return this.ListaDeCarros
+    }
+}
+
+class Pessoa {
+    private nome: string
+    private carroPreferido: string
+    private carro: any
+
+    constructor(nome: string, carroPreferido: string) {
+        this.nome = nome
+        this.carroPreferido = carroPreferido 
+    }
+
+    public dizerNome(): string {
+        return this.nome
+    }
+
+    public dizerCarroPreferido(): string {
+        return this.carroPreferido 
+    }
+
+    public comprarCarro(carro: any): void {
+        this.carro = carro
+    }
+
+    public dizerCarroQueTem(): any {
+        return this.carro
+    }
+}
+
+// Criar carros
+let carroA = new Carro('Dodge Journey', 4)
+let carroB = new Carro('Veloster', 3)
+let carroC = new Carro('Cerato', 4)
+
+/*
+    Agora... Colocar esses objetos dentro de um array
+    e... associar esse array de objetos do tipo carro
+    ao atributo lista de carros do objeto objeto concessionária
+*/
+// Criar array, montar a lista de carros da concessionária
+let listaDeCarros: Array<Carro> = [ carroA, carroB, carroC ]
+let concessionária = new Concessionaria('Rua dos Tabajaras', listaDeCarros)
+
+// exibir lista de carros
+console.log(concessionária.mostrarListaDeCarros())
